4cace88cc06899a96ecddc43ab66591c098ade51
Trent M <trent.moore.prog@gmail.com>
2025-07-29 18:03:38 -0600

    Add User table and UserRecipe junction table

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from config import db, ma
 from datetime import datetime
-from marshmallow.fields import String, DateTime, Integer, Nested
+from marshmallow.fields import String, DateTime, Integer, Nested, Email
 from marshmallow import validate
 
 
@@ -11,10 +11,11 @@ class Recipe(db.Model):
     prep_time = db.Column(db.Integer, index=True, nullable=False)
     cook_time = db.Column(db.Integer, index=True, nullable=False)
     servings = db.Column(db.Integer, index=True, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.now(), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now, nullable=False)
 
     recipe_ingredients = db.relationship('RecipeIngredient',
                                          back_populates='recipe')
+    collected_recipes = db.relationship('UserRecipe', back_populates='recipe')
 
 
 class Ingredient(db.Model):
@@ -24,6 +25,29 @@ class Ingredient(db.Model):
 
     recipe_ingredients = db.relationship('RecipeIngredient',
                                          back_populates='ingredient')
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(40), index=True, unique=True,
+                         nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    collected_recipes = db.relationship('UserRecipe', back_populates='user')
+
+
+class UserRecipe(db.Model):
+    __tablename__ = 'user_recipe'
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'),
+                          primary_key=True)
+    user_notes = db.Column(db.Text, nullable=True)
+    collected_at = db.Column(db.DateTime, default=datetime.now,
+                             nullable=False)
+
+    user = db.relationship('User', back_populates='collected_recipes')
+    recipe = db.relationship('Recipe', back_populates='collected_recipes')
 
 
 class RecipeIngredient(db.Model):
